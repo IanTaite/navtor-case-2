31fda47bd4f1b09a3896f7d5cbccbea6ca76fdec
Ian Taite <ian.taite@gmail.com>
2025-10-03 12:03:55 +0100

    add and configure NGRX Store and Effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideStore({}), // Empty root store - features will be provided in page components
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideRouter(routes)
   ]
 };
