5a23f9461fa61915b0961c4384e0427375edebe6
Ian Taite <ian.taite@gmail.com>
2025-10-03 12:28:34 +0100

    add and configure Ag-Grid

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+// Register all AgGrid Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
